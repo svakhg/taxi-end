04a184584b0f8cdab952c9d065d20736698db52b
athik13 <athik.13@gmail.com>
2017-12-31 12:46:41 +0500

    edit commit

---
+++ resources/views/configure/company/edit.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<ul class="breadcrumb">
+    <li><a href="{{ url('home') }}">Home</a></li>
+    <li><a href="#">Configure</a></li>
+    <li><a href="{{ url('configure/company') }}">Company</a></li>
+    <li class="active">Edit</li>
+</ul>
+
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        <h3 class="panel-title">Company Edit</h3>
+    </div>
+    <div class="panel-body">
+        <div class="row">       
+            <div class="col-md-12">
+                <div class="flash-message">
+                    @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+                        @if(Session::has('alert-' . $msg))
+
+                        <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
+                        
+                        @endif
+                    @endforeach
+                </div>
+            </div>         
+            <div class="col-md-12">
+                {!! form($form) !!}
+            </div> 
+        </div>
+        
+    </div>        
+</div>
+@endsection
