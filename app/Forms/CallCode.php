66f0a1075d599f2ae5cf5faf75b2029fe75307dd
athik13 <athik.13@gmail.com>
2017-12-31 11:51:13 +0500

    forms

---
+++ app/Forms/CallCode.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Forms;
+
+use Kris\LaravelFormBuilder\Form;
+
+class CallCode extends Form
+{
+    public function buildForm()
+    {
+        $this
+            ->add('center_id', 'text')
+            ->add('callCode', 'text');
+    }
+}
---
+++ app/Forms/Driver.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Forms;
+
+use Kris\LaravelFormBuilder\Form;
+
+class Driver extends Form
+{
+    public function buildForm()
+    {
+        $this
+            ->add('taxi_id', 'text')
+            ->add('driverName', 'text')
+            ->add('driverIdNo', 'text')
+            ->add('driverTempAdd', 'text')
+            ->add('driverPermAdd', 'text')
+            ->add('driverName', 'text')
+            ->add('driverEmail', 'text')
+            ->add('driverLicenceNo', 'text')
+            ->add('driverLicenceExp', 'text')
+            ->add('driverPermitNo', 'text')
+            ->add('driverPermitExp', 'date');
+    }
+}
