73d80894d835c9a1f8247d673bc42cdd99701c6c
Yaniu09 <imperialaeiou@gmail.com>
2017-12-31 13:03:50 +0600

    sadasd

---
+++ resources/views/configure/callcode/add.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<ul class="breadcrumb">
+    <li><a href="{{ url('home') }}">Home</a></li>
+    <li><a href="#">Configure</a></li>
+    <li><a href="{{ url('configure/call-code') }}">Call Code</a></li>
+    <li class="active">Add</li>
+</ul>
+
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        <h3 class="panel-title">Call Code</h3>
+    </div>
+    <div class="panel-body">
+        <div class="row">                
+            <div class="col-md-12">
+                {!! form($form) !!}
+            </div> 
+        </div>
+        
+    </div>        
+</div>
+@endsection
