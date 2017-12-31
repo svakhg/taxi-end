0a2df0e4fe68051db2d70fc70ba03d7883c6821a
Yaniu09 <imperialaeiou@gmail.com>
2017-12-31 14:33:57 +0600

    asdasdasd

---
+++ resources/views/configure/taxi/view.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<ul class="breadcrumb">
+    <li><a href="{{ url('home') }}">Home</a></li>
+    <li><a href="#">Configure</a></li>
+    <li><a href="{{ url('configure/taxi') }}">Taxi</a></li>
+    <li class="active">Taxi View</li>
+</ul>
+
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        <h3 class="panel-title">Taxi View</h3>
+    </div>
+    <div class="panel-body">
+        <div class="row">          
+            <div class="col-md-12">
+              <table>
+                  <thead>
+
+                  </thead>
+                  <tbody>
+                      
+                  </tbody>
+              </table>
+            </div> 
+        </div>
+        
+    </div>        
+</div>
+@endsection
