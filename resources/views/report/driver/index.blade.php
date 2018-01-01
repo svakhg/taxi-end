976a386ce880aa838d399d7f8d81339f4dff18d5
Yaniu09 <imperialaeiou@gmail.com>
2018-01-01 14:36:32 +0600

    fgdg

---
+++ resources/views/report/driver/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+<ul class="breadcrumb">
+    <li><a href="{{ url('home') }}">Home</a></li>
+    <li><a href="#">Configure</a></li>
+    <li><a href="{{ url('configure/taxi') }}">Taxi</a></li>
+    <li class="active">Driver Driver Reports</li>
+</ul>
+
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        <h3 class="panel-title">Driver Reports</h3>
+    </div>
+    <div class="panel-body">
+        <div class="row">          
+            <div class="col-md-12">
+                <table class="table table-striped">
+                        <thead>
+                            <th>taxi_id</th>
+                            <th>driverName</th>
+                            <th>driverIdNo</th>
+                            <th>driverTempAdd</th>
+                            <th>driverPermAdd</th>
+                            <th>driverMobile</th>
+                            <th>driverEmail</th>
+                            <th>driverLicenceNo</th>
+                            <th>driverLicenceExp</th>
+                            <th>driverPermitNo</th>
+                            <th>driverPermitExp</th>
+                        </thead>
+                        <tbody>
+                            @foreach ($drivers as $driver)
+                            <tr>
+                            <td>{{ $driver->taxi_id }}</td>
+                            <td>{{ $driver->driverName }}</td>
+                            <td>{{ $driver->driverIdNo }}</td>
+                            <td>{{ $driver->driverTempAdd }}</td>
+                            <td>{{ $driver->driverPermAdd }}</td>
+                            <td>{{ $driver->driverMobile }}</td>
+                            <td>{{ $driver->driverEmail }}</td>
+                            <td>{{ $driver->driverLicenceNo }}</td>
+                            <td>{{ $driver->driverLicenceExp }}</td>
+                            <td>{{ $driver->driverPermitNo }}</td>
+                            <td>{{ $driver->driverPermitExp }}</td>
+                            </tr>
+                            @endforeach
+                            
+                        </tbody>
+                </table>
+            </div>
+            
+        </div>
+    </div>        
+</div>
+@endsection
---
+++ resources/views/report/taxi/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+<ul class="breadcrumb">
+    <li><a href="{{ url('home') }}">Home</a></li>
+    <li><a href="#">Configure</a></li>
+    <li><a href="{{ url('configure/taxi') }}">Taxi</a></li>
+    <li class="active">Taxi View</li>
+</ul>
+
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        <h3 class="panel-title">Taxi View</h3>
+    </div>
+    <div class="panel-body">
+        <div class="row">          
+            <div class="col-md-12">
+              <table class="table table-striped">
+                  <thead>
+                        <th>callcode_id</th>
+                        <th>taxiNo</th>
+                        <th>taxiOwnerName</th>
+                        <th>taxiOwnerMobile</th>
+                        <th>taxiOwnerEmail</th>
+                        <th>taxiOwnerAddress</th>
+                        <th>registeredDate</th>
+                        <th>anualFeeExpiry</th>
+                        <th>roadWorthinessExpiry</th>
+                        <th>insuranceExpiry</th>
+                        <th>rate</th>
+                  </thead>
+                  <tbody>
+                      @foreach ($taxis as $taxi)
+                          <tr>
+                                <td>{{ $taxi->callcode_id }}</td>
+                                <td>{{ $taxi->taxiNo }}</td>
+                                <td>{{ $taxi->taxiOwnerName }}</td>
+                                <td>{{ $taxi->taxiOwnerMobile }}</td>
+                                <td>{{ $taxi->taxiOwnerEmail }}</td>
+                                <td>{{ $taxi->taxiOwnerAddress }}</td>
+                                <td>{{ $taxi->registeredDate }}</td>
+                                <td>{{ $taxi->anualFeeExpiry }}</td>
+                                <td>{{ $taxi->roadWorthinessExpiry }}</td>
+                                <td>{{ $taxi->insuranceExpiry }}</td>
+                                <td>{{ $taxi->rate }}</td>
+                          </tr>    
+                      @endforeach
+                        
+                  </tbody>
+              </table>
+            </div> 
+          
+        </div>
+    </div>        
+</div>
+@endsection
