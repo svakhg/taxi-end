eddac18f2ae45caadfcdad9e18a11ca884051b52
Yaniu09 <imperialaeiou@gmail.com>
2018-01-24 13:55:18 +0600

    driving School

---
+++ app/Http/Controllers/DrivingSController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DrivingS;
+use Illuminate\Http\Request;
+
+class DrivingSController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\DrivingS  $drivingS
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DrivingS $drivingS)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\DrivingS  $drivingS
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DrivingS $drivingS)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\DrivingS  $drivingS
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DrivingS $drivingS)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\DrivingS  $drivingS
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DrivingS $drivingS)
+    {
+        //
+    }
+}
---
+++ database/migrations/2018_01_24_071605_create_driving_s_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDrivingSTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('driving_s', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('id_card')->nullable();
+            $table->integer('phone')->nullable();
+            $table->string('category')->nullable();
+            $table->string('c_address')->nullable();
+            $table->string('p_address')->nullable();
+            $table->string('instructor')->nullable();
+            $table->integer('rate')->nullable();
+            $table->string('remarks')->nullable();
+            $table->string('finisheddate')->nullable();
+            $table->string('photo_url')->nullable();
+            $table->string('licence_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('driving_s');
+    }
+}
