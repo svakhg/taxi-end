99c2e6acc23885aaa5b48c7df8915aa44c109951
Yaniu09 <imperialaeiou@gmail.com>
2018-01-25 09:15:53 +0600

    sada

--- resources/views/drivingschool/index.blade.php
+++ resources/views/drivingschool/index.blade.php
@@ -1,20 +1,5 @@
 @extends('layouts.app')
 
-@section('css')
-<style>
-        #myInput {
-            background-image: url('/searchicon.png');
-            background-position: 10px 12px; 
-            background-repeat: no-repeat; 
-            width: 100%;
-            font-size: 16px;
-            padding: 12px 20px 12px 40px; 
-            border: 1px solid #ddd; 
-            margin-bottom: 12px; 
-        }
-</style>
-@endsection
-
 @section('content')
 
 <ul class="breadcrumb">
@@ -44,7 +29,7 @@
                     <div class="btn-group">
                         <button class="btn btn-success" onclick="printDiv('printableArea')"><i class="fa fa-print" aria-hidden="true"></i> Print the Page</button>
                         <button type="button" class="btn btn-primary"><a href="{{ url('driving-school/create') }}" style="color:white;"><i class="fa fa-user-plus" aria-hidden="true"></i> Register a new Student</a></button>
-                        <button type="button" class="btn btn-warning">Export To Excel</button>
+                        <button type="button" class="btn btn-warning" onclick="excelGen()">Export To Excel</button>
                     </div>         
                     <div style="margin-top:10px;">
                     </div>
@@ -64,23 +49,23 @@
                                         <th>Theory Test</th>
                                         <th>Joined on</th>
                                         <th>Registered By</th>
-                                        <th>Actions</th>
+                                        <th class="noprint">Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     @foreach($students as $student)
                                     <tr>
-                                        <td>{{ $student->name }}</th>
-                                        <td>{{ $student->id_card }}</td>
-                                        <td>{{ $student->phone }}</td>
-                                        <td>{{ $student->category }}</td>
-                                        <td>{{ $student->instructor }}</td>
-                                        <td>{{ $student->remarks }}</td>
-                                        <td>{{ $student->finisheddate }}</td>
-                                        <td>{{ $student->theorydate }}</td>
-                                        <td>{{ $student->created_at->toFormattedDateString() }}</td>
-                                        <td>{{ $student->user->name }}</td>
-                                        <td>
+                                        <td class="verticalAlign">{{ $student->name }}</th>
+                                        <td class="verticalAlign">{{ $student->id_card }}</td>
+                                        <td class="verticalAlign">{{ $student->phone }}</td>
+                                        <td class="verticalAlign">{{ $student->category }}</td>
+                                        <td class="verticalAlign">{{ $student->instructor }}</td>
+                                        <td class="verticalAlign">{{ $student->remarks }}</td>
+                                        <td class="verticalAlign">{{ $student->finisheddate }}</td>
+                                        <td class="verticalAlign">{{ $student->theorydate }}</td>
+                                        <td class="verticalAlign">{{ $student->created_at->toFormattedDateString() }}</td>
+                                        <td class="verticalAlign">{{ $student->user->name }}</td>
+                                        <td class="noprint">
                                             <a style="margin:1px" class="btn btn-warning" href="{{ url()->current() }}/students/{{ $student->id }}/edit">Edit</a>
                                             <a style="margin:1px" class="btn btn-success" href="{{ url()->current() }}/students/{{ $student->id }}">Reciept</a>
                                         </td>
@@ -131,5 +116,104 @@
             
                 document.body.innerHTML = originalContents;
             }
+
+            function excelGen() {
+                $("#printableArea").table2excel({
+                    exclude: ".excludeThisClass",
+                    name: "TDS RECORDS 2018 ",
+                    filename: "TDS_RECORDSHEET_2018.xls" 
+                });
+            }
         </script>
+        <script src="//cdn.rawgit.com/rainabba/jquery-table2excel/1.1.0/dist/jquery.table2excel.min.js"></script>
+@endsection
+
+@section('css')
+
+<style>
+
+    #myInput {
+        background-image: url('/searchicon.png');
+        background-position: 10px 12px; 
+        background-repeat: no-repeat; 
+        width: 100%;
+        font-size: 16px;
+        padding: 12px 20px 12px 40px; 
+        border: 1px solid #ddd; 
+        margin-bottom: 12px; 
+    }
+    
+    .verticalAlign {
+        vertical-align: middle !important;
+    }
+        
+    /* Print styling */
+    @media print {
+        [class*="col-sm-"] {
+            float: left;
+    }
+        [class*="col-xs-"] {
+            float: left;
+    }
+        .col-sm-12, .col-xs-12 {
+            width:100% !important;
+    }
+        .col-sm-11, .col-xs-11 {
+            width:91.66666667% !important;
+    }
+        .col-sm-10, .col-xs-10 {
+            width:83.33333333% !important;
+    }
+        .col-sm-9, .col-xs-9 {
+            width:75% !important;
+    }
+        .col-sm-8, .col-xs-8 {
+            width:66.66666667% !important;
+    }
+        .col-sm-7, .col-xs-7 {
+            width:58.33333333% !important;
+    }
+        .col-sm-6, .col-xs-6 {
+            width:50% !important;
+    }
+        .col-sm-5, .col-xs-5 {
+            width:41.66666667% !important;
+    }
+        .col-sm-4, .col-xs-4 {
+            width:33.33333333% !important;
+    }
+        .col-sm-3, .col-xs-3 {
+            width:25% !important;
+    }
+        .col-sm-2, .col-xs-2 {
+            width:16.66666667% !important;
+    }
+        .col-sm-1, .col-xs-1 {
+            width:8.33333333% !important;
+    }
+        .col-sm-1, .col-sm-2, .col-sm-3, .col-sm-4, .col-sm-5, .col-sm-6, .col-sm-7, .col-sm-8, .col-sm-9, .col-sm-10, .col-sm-11, .col-sm-12, .col-xs-1, .col-xs-2, .col-xs-3, .col-xs-4, .col-xs-5, .col-xs-6, .col-xs-7, .col-xs-8, .col-xs-9, .col-xs-10, .col-xs-11, .col-xs-12 {
+            float: left !important;
+    }
+        body {
+            margin: 0;
+            padding: 0 !important;
+            min-width: 768px;
+    }
+        .container {
+            width: auto;
+            min-width: 750px;
+    }
+        body {
+            font-size: 10px;
+    }
+        a[href]:after {
+            content: none;
+    }
+        .noprint, div.alert, header, .group-media, .btn, .footer, form, #comments, .nav, ul.links.list-inline, ul.action-links {
+            display:none !important;
+    }
+    }
+
+</style>    
+
 @endsection
