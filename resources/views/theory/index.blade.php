324a71822256b61f79fd5fc3f5e257e97c74eec6
athik13 <athik.13@gmail.com>
2018-03-17 19:43:39 +0500

    tyjyhtgrfe

--- resources/views/theory/index.blade.php
+++ resources/views/theory/index.blade.php
@@ -8,53 +8,64 @@
             
             <div class="panel panel-success">
                 <div class="panel-body">
-                    <center>
-                        <h2 class="dhivehi-font">{{ $quiz->name }}</h2>
-                    </center>
+                    <div class="first-panel">
+                        <center>
+                            <img src="/bismilah.jpg" height="90px">
+                            <br>
+                            <img src="/Taviyani_Logo.png" height="250px">
+                            <hr>
+                            <span style="font-size: 60px" class="dhivehi-font">{{ $quiz->name }}</span>
+                            <hr>
+                            <a name="" id="" class="btn btn-primary btn-lg dhivehi-font" href="#question1" role="button">
+                                ޓެސްޓް ފަށާ
+                            </a>
+                        </center>
+                    </div>
                     <hr>
-                    <form action="{{ url()->current() }}/post" class="dhivehi-font" method="POST" role="form">
-                        {{ csrf_field() }}
-                        <?php $i = 0 ?>
-                        @foreach ($quiz->questions->shuffle() as $question)
-                            <?php $i++ ?>
-                            <fieldset class="question" id="question{{ $i }}">
-                                <h1 class="">{{ $i }}) {{ $question->body }}</h1>
-                                <div class="answers">
-                                    @foreach ($question->answers->shuffle() as $answer)
-                                        @if ($answer->photo_url)
-                                            <img src="{{ \App\Helpers\Helper::s3_url_gen($answer->photo_url) }}" alt="">
+                    <div class="questions">
+                        <form action="{{ url()->current() }}/post" class="dhivehi-font" method="POST" role="form">
+                            {{ csrf_field() }}
+                            <?php $i = 0 ?>
+                            @foreach ($quiz->questions->shuffle() as $question)
+                                <?php $i++ ?>
+                                <fieldset class="question" id="question{{ $i }}">
+                                    <h1 class="">{{ $i }}) {{ $question->body }}</h1>
+                                    <div class="answers">
+                                        @foreach ($question->answers->shuffle() as $answer)
+                                            @if ($answer->photo_url)
+                                                <img src="{{ \App\Helpers\Helper::s3_url_gen($answer->photo_url) }}" alt="">
+                                            @endif
+                                            <div class="radio">
+                                                <label class="" style="font-size: 25px">
+                                                    <input type="radio" name="question-{{ $question->id }}" value="{{ $answer->id }}" style="margin-top: 18px; margin-left: 10px;">
+                                                    <span class="answer">
+                                                        {{ $answer->answer }}
+                                                    </span>
+                                                </label>
+                                            </div>    
+                                        @endforeach
+                                    </div>
+                                    <div>
+                                        @if ($i !== $quiz->questions->count())
+                                            <a name="" id="" class="btn btn-primary" href="#question{{ $i + 1 }}" role="button">
+                                                Next
+                                            </a>
+                                        @else
+                                            <button type="submit" class="btn btn-primary">Submit</button>
                                         @endif
-                                        <div class="radio">
-                                            <label class="" style="font-size: 25px">
-                                                <input type="radio" name="question-{{ $question->id }}" value="{{ $answer->id }}" style="margin-top: 18px; margin-left: 10px;">
-                                                <span class="answer">
-                                                    {{ $answer->answer }}
-                                                </span>
-                                            </label>
-                                        </div>    
-                                    @endforeach
-                                </div>
-                                <div>
-                                    @if ($i !== $quiz->questions->count())
-                                        <a name="" id="" class="btn btn-primary" href="#question{{ $i + 1 }}" role="button">
-                                            Next
-                                        </a>
-                                    @else
-                                        <button type="submit" class="btn btn-primary">Submit</button>
-                                    @endif
-                                    @if ($i !== 1)
-                                        <a name="" id="" class="btn btn-info" href="#question{{ $i - 1 }}" role="button">
-                                            Previous
-                                        </a>
-                                    @endif
-                                </div>
-                            </fieldset>
-                            
-                            <hr style="border-top: 1px solid #000000;">
+                                        @if ($i !== 1)
+                                            <a name="" id="" class="btn btn-info" href="#question{{ $i - 1 }}" role="button">
+                                                Previous
+                                            </a>
+                                        @endif
+                                    </div>
+                                </fieldset>
+                                
+                                <hr style="border-top: 1px solid #000000;">
 
-                        @endforeach
-                    </form>
-                    
+                            @endforeach
+                        </form>
+                    </div>
 
                 </div>
             </div>
