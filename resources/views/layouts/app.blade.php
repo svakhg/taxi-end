3bbaa8720a3806b24642a821505b11ed208a6cb8
athik13 <athik.13@gmail.com>
2018-04-14 15:07:39 +0500

    re

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -159,7 +159,6 @@
                     <!-- Authentication Links -->
                     @if (Auth::guest())
                         <li><a href="{{ route('login') }}">Login</a></li>
-                        <li><a href="{{ route('register') }}">Register</a></li>
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
